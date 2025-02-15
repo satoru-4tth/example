21891ac3b6bcd1c325b220d812713828122a6bcd
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-02-15 22:38:58 +0900

    チャプター７のいちご700円サーブレット＆完成。

---
+++ example/src/main/java/ex/FruitServlet.java
@@ -0,0 +1,23 @@
+package ex;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/FruitServlet")
+public class FruitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Fruit fruit = new Fruit("いちご", 700);
+		request.setAttribute("fruit", fruit);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/ex/fruit.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
