b631aef4df15254a75dddbf3a2af5ddfe9a8e514
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-02-16 21:46:39 +0900

    前回リクエストスコープ経由から、セッションスコープ経由に変更。

--- example/src/main/java/ex/FruitServlet.java
+++ example/src/main/java/ex/FruitServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/FruitServlet")
 public class FruitServlet extends HttpServlet {
@@ -15,7 +16,8 @@ public class FruitServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Fruit fruit = new Fruit("いちご", 700);
-		request.setAttribute("fruit", fruit);
+		HttpSession session = request.getSession();
+		session.setAttribute("fruit", fruit);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/ex/fruit.jsp");
 		dispatcher.forward(request, response);
 	}
--- example/src/main/webapp/WEB-INF/ex/fruit.jsp
+++ example/src/main/webapp/WEB-INF/ex/fruit.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ page import="ex.Fruit" %>
-<% Fruit fruit = (Fruit)request.getAttribute("fruit"); %>
+<% Fruit fruit = (Fruit)session.getAttribute("fruit"); %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +9,9 @@
 <title>700円のいちご</title>
 </head>
 <body>
+
 <p><%= fruit.getName() %>の値段は<%= fruit.getPrice() %>円ですよ。</p>
+
+
 </body>
 </html>
