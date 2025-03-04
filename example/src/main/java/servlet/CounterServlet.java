3cb69c0bba47480de0ab1013f1c4a8125e6bbb08
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-03-04 23:55:26 +0900

    init()とdestroy()を持つサーブレットクラス

--- example/src/main/java/servlet/CounterServlet.java
+++ example/src/main/java/servlet/CounterServlet.java
@@ -2,8 +2,9 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import jakarta.servlet.ServletContext;
+
 import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -34,18 +35,22 @@ public class CounterServlet extends HttpServlet {
 		application.setAttribute("count", count);
 		
 		// HTMLを出力
-		response.setCountType("text/html; charset=UTF-8");
-		PrintWriter out  response.getWriter();
-		out.println
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"UTF-8\" />");
+		out.println("<title>訪問回数を表示</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>訪問回数：" + count + "</p>");
+		out.println("<a href=\"CounterServlet\">更新</a>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 	public void destroy() {
-		// TODO Auto-generated method stub
+		System.out.println("destroy()が実行されました");
 	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-
-
 }
