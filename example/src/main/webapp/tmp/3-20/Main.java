88c7d11051ea3baa11d484235c23983c9c486bd8
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-03-08 16:43:50 +0900

    javaの問題コード

---
+++ example/src/main/webapp/tmp/3-20/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		String country = "US FR JP".substring(3, 5);
+		String currency = switch (country); {
+			case "US": yield "USD";
+			case "DE", "FR": yield "EUR";
+			case "JP": yield "JPY";
+			case "UK": yield "GBP";
+		};
+		System.out.println(currency);
+	}
+}
