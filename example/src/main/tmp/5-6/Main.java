d7c47c8300fbdf5cbf0fa07d7feda94a499e88be
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-03-08 22:48:38 +0900

    3-6

---
+++ example/src/main/tmp/5-6/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Main obj = new Main();
+		double v = obj.x(10);
+		
+	}
+	// public float x(int a) { return a *0.5F;}
+	// public void x(int b) { return 100.0;}
+	// public double x(int...c) { double x = c[0];}
+	// public void x(int x, double y) { double z = x + y;}
+	// public double x() { return 10.0;}
+}
