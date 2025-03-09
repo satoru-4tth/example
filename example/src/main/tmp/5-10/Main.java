3b08157ae0b1611c5041d61507b3943ab6329d3e
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-03-09 17:33:37 +0900

    5-10

---
+++ example/src/main/tmp/5-10/Main.java
@@ -0,0 +1,21 @@
+public class Employee {
+	int id;
+	String name;
+	public Employee(int id) {
+		this.id = id;
+	}
+	public Employee(String name, int id) {
+		this(id);
+		this.name = name;
+	}
+	public void showEmployeeInfo() {
+		System.out.println(id + ";" + name);
+	}
+}
+class Main {
+	public static void main(String[] args) {
+		Employee emp = new Employee("Duke", 0);
+		emp.showEmployeeInfo();
+	}
+}
+
