c7aa833279688ab52a75b95912d7d4fefc4a55a3
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-03-09 20:26:47 +0900

    5-13途中

---
+++ example/src/main/tmp/5－13/Sample.java
@@ -0,0 +1,16 @@
+public class Sample {
+	public Sample() {
+		this(1);
+		System.out.print(0);
+	}
+	public Sample(int x) {
+		this(x, 2);
+		this();
+		System.out.print(x);
+	}
+	public Sample(int x, int y) {
+		System.out,println(x + "" + y);
+	}
+    public static void main {
+    }
+}
