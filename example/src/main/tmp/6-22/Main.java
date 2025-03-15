8c5e0af67a49a157dbca5a2a1984bf24a93656f0
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-03-15 22:59:38 +0900

    更新

---
+++ example/src/main/tmp/6-22/Main.java
@@ -0,0 +1,12 @@
+inport java.util.HashMap;
+inport java.util.Map;
+public class Main {
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<>();
+		map.add("UK", "United kingdom");
+		map.add("DE", "Germany");
+		map.add("FR", "France");
+		System.out.println(map);
+	}
+
+}
