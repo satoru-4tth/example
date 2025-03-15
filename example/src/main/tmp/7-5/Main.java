452d1ce1ed74a5b203462e72f859e530b67e1677
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-03-16 01:00:11 +0900

    7章

---
+++ example/src/main/tmp/7-5/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			int a 10, b = 0;
+			int result = a / b;
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage() + ":" + a ":" + b);
+		}
+	}
+
+}
