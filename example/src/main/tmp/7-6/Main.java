1e4795777e2312febdaa3ea4853d7a6e49269c0f
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-03-16 01:15:25 +0900

    7-6

---
+++ example/src/main/tmp/7-6/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int[] values = {1, 2, 3, 4};
+		try {
+			disp(values);
+			System.out.println(" try-end");
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.print(" catch");
+		} finally {
+			System.out.print(" finally");
+		}
+	}
+	public static void disp(int[] x); {
+	  for (int i = 0; i <= 4 ; i++) {
+		  System.out.println(x[i]);
+	  }
+   }
+}
