ade163e5897a2d9ac620cca8d9568de8e00c56c4
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-03-16 00:24:13 +0900

    7章

---
+++ example/src/main/tmp/7-3/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		String[] values = {"x", "y", "z"};
+		for (int i = 0; i <= 3; i++) {
+			System.out.println(values[i]);
+		}
+	}
+}
---
+++ example/src/main/tmp/7-4/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		String s = null;
+		try {
+			int i = Integer.parseInt(s);
+			System.out.println(s);
+		} catch ( /* insert code here */ ) {
+			System.out.println("exception");
+		}
+	}
+}
