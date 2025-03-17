c686ab8e6a20ecf5c309cd2c06a3bffe7c30f644
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-03-17 23:47:32 +0900

    2-14

---
+++ example/src/main/tmp/2-14/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String[][] letters = {{"A ", "B ", "C ", "D "}
+                            , {"E ", "F ", "G ", "H "}};
+        for (String[] letter : letters) {
+            // insert code here
+        }
+    }
+}
+
+
+/*
+ * A for (String l : letters) { System.out.print(l); }
+ * B for (String[] l : letters) { System.out.print(l); }
+ * C for (String l : letter) { System.out.print(l); }
+ * D for (int i = 0; i < letters.length; i++) { System.out.print(letters[i]); }
+ * E for (int i = 0; i < letter[i].length; i++) { System.out.print(letter[i]); }
+ */
