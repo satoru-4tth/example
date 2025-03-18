ac5a6c573e3065856f1aa5f00d3e7f607bf32279
konumasatoru <satorukonuma_0604@yahoo.co.jp>
2025-03-19 00:23:39 +0900

    5-11

---
+++ example/src/main/tmp/5-11/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public void foo(int x) {}
+    // insert code here
+}
+
+/*
+A. void foo(integer... x) {}
+B. public void bar(int x) {}
+C. boolean foo(int x, int y) { return false; }
+D. public boolean bar(var x, var y) { return false; }
+E. public int foo(String s, int x) { return s.length() + x; }
+F. public String foo(int i) { return String.valueOf(); }
+*/
