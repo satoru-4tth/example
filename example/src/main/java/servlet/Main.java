2472f65a76ac647778a589287fe1e3a2194f3dc7
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-03-23 05:34:27 +0900

    途中

---
+++ example/src/main/java/servlet/Main.java
@@ -0,0 +1,9 @@
+package servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("Main")
+public class Main extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+}
