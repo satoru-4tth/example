ac72396bd749dc702745a05553b5830ff1d7a961
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-04-01 08:23:26 +0900

    11.3.2 理解不足の途中

--- example/src/main/java/servlet/FormServlet.java
+++ example/src/main/java/servlet/FormServlet.java
@@ -1,1 +1,21 @@
 package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/Formservlet")
+public class FormServlet extends HttoServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+		// リクエストパラメータ取得
+		// request.setCharacterEncoding("UTF-8")
+		String name = request.getParameter("name");
+		String gender = request.getParameter("gender");
+	}
+}
