a82c2d37be1036caddddcfbfd5a840789b70f175
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-04-04 12:33:16 +0900

    a

--- example/src/main/tmp/kurohon/3-33/Sample.java
+++ example/src/main/tmp/kurohon/3-33/Sample.java
@@ -3,8 +3,8 @@ public class Sample {
 	public static void main(String[] args) {
 		int array[][] = new int[][] {{ 1, 2}, {2, 3, 4}};
 		int total = 0;
-		for (int i = 0; i < array.lenght; i++) {
-			for (int j = i; j < array[i].lenght; j++) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i; j < array[i].length; j++) {
 				total = array[i][j];
 			}
 		}
