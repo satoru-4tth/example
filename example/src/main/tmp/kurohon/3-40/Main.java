5d6595cc1dce6c6318ce21df6a95e4169d926e56
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-04-04 14:45:22 +0900

    3-40

---
+++ example/src/main/tmp/kurohon/3-40/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		String[] array = { "A", "B" };
+		for (String a : array) {
+			for (String b : array) {
+				if ("B".equals(b))
+					break;
+				System.out.print(b);
+			}
+		}
+	}
+
+}
