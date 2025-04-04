6abd92f78bc4269b35fe1fec43a82785c06b13b3
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-04-04 15:11:54 +0900

    修正

--- example/src/main/tmp/kurohon/3-43/Main.java
+++ example/src/main/tmp/kurohon/3-43/Main.java
@@ -3,7 +3,7 @@ public class Main {
 		int total = 0;
 		a: for (int i = 0; i < 5; j++) {
 			b: for (int j = 0; j < 5; j++) {
-				if (i % 2 == 0) countinue a;
+				if (i % 2 == 0) continue a;
 				if (3 < j) break b;
 				total += j;
 			}
