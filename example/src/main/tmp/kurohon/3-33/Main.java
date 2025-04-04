670a9f7a615c832511090078d8d79a2d3e5d2df7
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-04-04 13:02:02 +0900

    a

--- example/src/main/tmp/kurohon/3-33/Main.java
+++ example/src/main/tmp/kurohon/3-33/Main.java
@@ -1,12 +1,12 @@
 public class Main {
-  public static void main(String[] args) {
-	  int array[][] = new int[][] {{1, 2}, {2, 3, 4}};
-	  int total = 0;
-	  for (int i = 0; i < array.length; i++) {
-	    for (int j = i; j < array[i].length; j++) {
-	    total += array[i][j];
-        }
-      }
-      System.out.println(total);
-      }
-  }
+	public static void main(String[] args) {
+		int array[][] = new int[][] {{1, 2}, {2, 3, 4}};
+		int total = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i; j < array[i].length; j++) {
+				total += array[i][j];
+			}
+		}
+		System.out.println(total);
+	}
+}
