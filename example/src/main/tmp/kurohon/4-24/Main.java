ea9ec51a2e3bc9f458d1ef7d642fb47a6dab6ca4
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-04-08 14:47:55 +0900

    4-24

---
+++ example/src/main/tmp/kurohon/4-24/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		Sample s = new Sample();
+	}
+
+}
---
+++ example/src/main/tmp/kurohon/4-24/Sample.java
@@ -0,0 +1,17 @@
+public class Sample {
+	public Sample() {
+		#####
+	}
+	public Sample(String str, int num) {
+		System.out.println("ok.");
+	}
+
+}
+
+
+/*
+A. Sample(null, 0);
+B. this(null, 0);
+C. super(null, 0);
+D. this.Sample(null, 0);
+*/
