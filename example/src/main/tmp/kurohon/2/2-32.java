6bd6a8349538596a5d3315d62dcb0b78f7a3f335
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-04-11 18:58:14 +0900

    追記

---
+++ example/src/main/tmp/kurohon/2/2-32.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		String[] array = {"A", "B", "C", "D"};
+		array[0] = null;
+		for (String str : array) {
+			System.out.print(str);
+		}
+	}
+}
