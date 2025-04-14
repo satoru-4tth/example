80c39b1b9f0800c8d9ab14abd02f822fe126f1c9
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-04-14 16:43:46 +0900

    totyuu

---
+++ example/src/main/tmp/kurohon/6-3/Main.java
@@ -0,0 +1,14 @@
+public class SampleException extends Exception {}
+
+public class SubSampleException extends SampleException {}
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
+
+
+public class Main {
+
+}
