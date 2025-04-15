df0bd9541ff37f2b9c5c53bcc56ff2afd451f699
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-04-15 17:32:53 +0900

    s

---
+++ example/src/main/tmp/kurohon/2/2-31/Main.java
@@ -0,0 +1,17 @@
+public class Item {
+	String name;
+	int price = 100;
+}
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		Item[] items = new Item[3];
+		int total = 0;
+		for (int i = 0; i < items.length; i++) {
+			total += items[i].price;
+		}
+		System.out.println(total);
+	}
+}
