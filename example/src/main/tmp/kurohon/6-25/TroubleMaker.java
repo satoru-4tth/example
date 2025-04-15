9bd805102426582dfcf9f556760c9088b8f142d0
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-04-15 10:42:17 +0900

    6-25

---
+++ example/src/main/tmp/kurohon/6-25/TroubleMaker.java
@@ -0,0 +1,19 @@
+public class TroubleMaker implements AutoCloseable {
+	@Override
+	public void close() throws Exception {
+		throw new RuntimeException("trouble");
+	}
+}
+
+
+public class TroubleMaker {
+	public static void main(String[] args) {
+		try (TroubleResource a = new TroubleResource()) {
+			throw new Execption();
+		} catch (RuntimeException e) {
+			System.out.println("A");
+		} catch (Exception e) {
+			System.out.println("B");
+		}
+	}
+}
