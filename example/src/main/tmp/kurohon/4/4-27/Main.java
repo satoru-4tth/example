0405546daa57a21dbb761cacc5a7f70f20126d2e
satoru-4tth <satorukonuma_0604@yahoo.co.jp>
2025-04-16 12:09:30 +0900

    a

--- example/src/main/tmp/kurohon/4/4-27/Main.java
+++ example/src/main/tmp/kurohon/4/4-27/Main.java
@@ -26,6 +26,6 @@ public class Main {
 	public static void main(String[] args) {
 		StoryBook story = new StoryBook();
 		story.setIsbn("xxx-x-xxxxxx-xx-x");
-		story.prinyInfo();
+		story.printInfo();
 	}
 }
